d4cf5d93af78eacb7f9ab7ccc19c40bf20426b49
JulienFosse <fossejul@gmail.com>
2020-09-07 15:09:43 +0200

    initnodejs

---
+++ api/router.js
@@ -0,0 +1,28 @@
+// Import
+const express = require('express'),
+    router = express.Router(),
+    path = require('path')
+
+// Controller
+const homeController = require('./controllers/homeController'),
+    articleController = require('./controllers/articleController'),
+    contactController = require('./controllers/contactController'),
+    aboutController = require('./controllers/aboutController');
+
+// Home
+router.route('/')
+    .get(homeController.get)
+
+// Article
+router.route('/article')
+    .get(articleController.get)
+
+// Contact
+router.route('/contact')
+    .get(contactController.get)
+
+// About
+router.route('/about')
+    .get(aboutController.get)
+
+module.exports = router;
